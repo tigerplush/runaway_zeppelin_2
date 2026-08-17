75d51c117d290997c36974e5dcf4b7883ce7c551
tigerplush <christopher.putz@corgibase.de>
2026-08-17 11:00:36 +0200

    added primitive in-game-time

---
+++ src/in_game_time.rs
@@ -0,0 +1,62 @@
+use bevy::prelude::*;
+use chrono::*;
+
+const STARTING_DATE: &str = "1928-10-11 08:00:00";
+const FROM_STRING_FMT: &str = "%Y-%m-%d %H:%M:%S";
+const TO_STRING_FMT: &str = "%A, %_H:%M %_d %B %Y";
+const DEFAULT_TIME_SPEED_FACTOR: u32 = 48;
+
+/// Represents the in-game time of a singular run.
+#[derive(Reflect, Resource)]
+#[reflect(Resource)]
+pub struct InGameTime {
+    #[reflect(ignore)]
+    current_time: NaiveDateTime,
+    #[reflect(ignore)]
+    starting_time: NaiveDateTime,
+    time_speed_factor: u32,
+}
+
+impl InGameTime {
+    /// Creates a new InGameTime from the given date and time.
+    pub fn new(date: NaiveDate, time: NaiveTime, time_speed_factor: u32) -> Self {
+        let date_time = NaiveDateTime::new(date, time);
+        Self {
+            current_time: date_time,
+            starting_time: date_time,
+            time_speed_factor,
+        }
+    }
+
+    /// Returns the number of days that have passed in game.
+    ///
+    /// Note: directly at start, 0 days have passed.
+    pub fn days(&self) -> i64 {
+        let diff = self.current_time - self.starting_time;
+        diff.num_days()
+    }
+}
+
+impl Default for InGameTime {
+    fn default() -> Self {
+        let date = NaiveDate::parse_from_str(STARTING_DATE, FROM_STRING_FMT);
+        let time = NaiveTime::parse_from_str(STARTING_DATE, FROM_STRING_FMT);
+        Self::new(date.unwrap(), time.unwrap(), DEFAULT_TIME_SPEED_FACTOR)
+    }
+}
+
+fn tick_in_game_time(time: Res<Time<Virtual>>, mut in_game_time: ResMut<InGameTime>) {
+    if time.is_paused() {
+        return;
+    }
+
+    let time_speed_factor = in_game_time.time_speed_factor;
+    in_game_time.current_time += time.delta() * time_speed_factor;
+}
+
+pub fn plugin(app: &mut App) {
+    app.register_type::<InGameTime>().add_systems(
+        Update,
+        tick_in_game_time.run_if(resource_exists::<InGameTime>),
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use bevy::prelude::*;
 
+mod in_game_time;
+
 /// Main Plugin for the whole game. This will add all other game-relevant
 /// plugins and base systems that are cross-cutting.
 pub struct AppPlugin;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(in_game_time::plugin);
     }
 }
